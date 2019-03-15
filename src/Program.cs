fd8fcee96a4eec4221eeb97df65aa7d226c0bde8
Dave Hillier <dave@hillier.co>
2019-03-15 14:42:26 +0000

    Added coordinate validation

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 [assembly: InternalsVisibleTo("Battleships19.Battleships19.Tests")]
 
@@ -12,19 +13,51 @@ namespace Battleships19
     {
       Game.Start(Console.Out, Console.In);
     }
-
-
-
-
   }
 
   public class Game
   {
+    const int BoardSize = 10;
+
     public static void Start(TextWriter @out, TextReader @in)
     {
       @out.WriteLine("Enter a coordinate to shoot: ");
-      var x = @in.ReadLine();
-      @out.WriteLine("ERROR: Invalid Coordinates");
+      var input = @in.ReadLine();
+
+      bool valid = TryParse(input);
+      if (!valid)
+        @out.WriteLine("ERROR: Invalid Coordinates");
+    }
+
+    private static bool TryParse(string input)
+    {
+      var rx = new Regex(@"([a-z]+)([0-9]+)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+      var match = rx.Match(input);
+
+      if (!match.Success || match.Groups.Count < 3)
+      {
+        return false;
+      }
+
+      var alpha = match.Groups[1].Value.ToLowerInvariant();
+      if (alpha.Length > 1)
+      {
+        return false;
+      }
+
+      var column = (int)alpha[0] - 'a';
+      if (column < 0 || column >= BoardSize)
+      {
+        return false;
+      }
+
+      var row = int.Parse(match.Groups[2].Value) - 1;
+      if (row < 0 || row >= BoardSize)
+      {
+        return false;
+      }
+
+      return true;
     }
   }
 }
