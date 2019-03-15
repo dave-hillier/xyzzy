f9fff6a8b8459063abc33b1fea91a72726796179
Dave Hillier <dave@hillier.co>
2019-03-15 14:08:44 +0000

    Failing test: Accept on grid

--- test/Battleships19.Tests/UnitTest1.cs
+++ test/Battleships19.Tests/UnitTest1.cs
@@ -21,5 +21,20 @@ namespace Battleships19.Tests
 
       Assert.StartsWith("ERROR", lines[1]);
     }
+
+    [Theory]
+    [InlineData("a1")]
+    [InlineData("A1")]
+    [InlineData("J1")]
+    [InlineData("J10")]
+    public void Accepts_coordinates_on_grid(string coords)
+    {
+      var writer = new StringWriter();
+      Game.Start(writer, new StringReader($"{coords}\n"));
+
+      var lines = writer.ToString().Split("\n");
+
+      Assert.DoesNotContain("ERROR", lines[1]);
+    }
   }
 }
