83c87215b29a16215df65050bf1dfe4eac38829a
Dave Hillier <dave@hillier.co>
2019-03-16 22:16:00 +0000

    single ship generate

--- src/RandomShipPosition.cs
+++ src/RandomShipPosition.cs
@@ -1,22 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace Battleships19
 {
-  public class RandomShipPosition
+  class RandomShipPosition
   {
+    public static List<int> Lengths = new List<int> { 5, 4, 4 };
+    private const string columns = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static Random rng = new Random();
-    public Func<string> NextOrientation = () => rng.Next(0, 2) == 1 ? "h" : "v";
-    public Func<(int, int)> NextCoordinates = () => (rng.Next(0, Game.BoardSize), rng.Next(0, Game.BoardSize));
+    public static Func<string> NextOrientation = () => rng.Next(0, 2) == 1 ? "h" : "v";
+    public static Func<(int column, int row)> NextCoordinates = () => (rng.Next(0, Game.BoardSize), rng.Next(0, Game.BoardSize));
+
+    private static HashSet<string> Horizontal((int column, int row) coord, int length)
+    {
+      return Enumerable.
+        Range(0, length).
+        Select(x => $"{columns[coord.column + x]}{1 + coord.row}").
+        ToHashSet();
+    }
+
+    private static HashSet<string> Vertical((int column, int row) coord, int length)
+    {
+      return Enumerable.
+        Range(0, length).
+        Select(x => $"{columns[coord.column]}{1 + coord.row + x}").
+        ToHashSet();
+    }
 
     public static List<HashSet<string>> Generate()
     {
-      return new List<HashSet<string>> {
-        new HashSet<string> { "A1", "A2", "A3", "A4" },
-        new HashSet<string> { "B1", "B2", "B3", "B4" },
-        new HashSet<string> { "C1", "C2", "C3", "C4", "C5" },
-      };
+      return Lengths.Select(l =>
+      {
+        var o = NextOrientation();
+        var c = NextCoordinates();
+
+        return o == "h" ? Horizontal(c, l) : Vertical(c, l);
+      }).ToList();
     }
   }
 }
---
+++ test/Battleships19.Tests/RandomShipPositionTest.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Battleships19.Tests
+{
+  public class RandomShipPositionTest
+  {
+    private List<string> orientations = new List<string> { "h", "h", "h" };
+    private List<(int, int)> startPositions = new List<(int, int)> { (0, 0), (1, 0), (2, 0) };
+
+    [Fact]
+    public void Place_single_ship_horizontal()
+    {
+      RandomShipPosition.NextOrientation = () => PopFirst(new List<string> { "h" });
+      RandomShipPosition.NextCoordinates = () => PopFirst(startPositions);
+      RandomShipPosition.Lengths = new List<int> { 5 };
+      var positions = RandomShipPosition.Generate();
+      Assert.Equal(positions.First(), new[] { "A1", "B1", "C1", "D1", "E1" });
+    }
+
+    [Fact]
+    public void Place_single_ship_vertical()
+    {
+      RandomShipPosition.NextOrientation = () => PopFirst(new List<string> { "v" });
+      RandomShipPosition.NextCoordinates = () => PopFirst(startPositions);
+      RandomShipPosition.Lengths = new List<int> { 5 };
+      var positions = RandomShipPosition.Generate();
+      Assert.Equal(positions.First(), new[] { "A1", "A2", "A3", "A4", "A5" });
+    }
+
+    private T PopFirst<T>(List<T> list)
+    {
+      var result = list.First();
+      list.RemoveAt(0);
+      return result;
+    }
+  }
+}
